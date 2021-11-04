d2fe9d602ba8205f86531fcd30b8eb23f054fba9
Jean Bravo <jeanbravo@MacBook-Air-de-Jean.local>
2021-11-04 13:14:12 +0100

    jum

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
 import React, { useState } from 'react'
 
-function App() {
-
-  const [value, setValue] = useState(10)
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState([])
 
   return (
-   
     <div>
-      {value}
-      <button onClick={() => setValue(0)}>reset to zero</button>
+      <h1>Give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>total</p>
+
     </div>
   )
 }
