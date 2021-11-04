4aded1d222f067c27fe7870bbe74e0c9c70b365b
jean bravo <jjbravo1996@gmail.com>
2021-11-04 17:11:52 +0100

    parte1 lista

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,17 +1,60 @@
-import './App.css';
 import React, { useState } from 'react'
 
-function App() {
+const Title = (props) => <h1>{props.name}</h1>
 
-  const [value, setValue] = useState(10)
+const setValue = (func, newValue) =>() =>{
+  func(newValue)
+}
+
+const Button = (props) => {
+  return(
+  <button onClick={setValue(props.func, props.value+1)}>
+    {props.name}
+  </button>
+  )
+}
+
+const Statistic = (props) => {
+  return(
+    <p>{props.name} {props.value}</p>
+  )
+}
+
+const Statistics = (props) => {
+  const{good, neutral, bad} = props
+  const all = good + neutral + bad
+  if(all === 0){
+    return(
+      <p>No feedbacks for now!</p>
+    )
+  }return(
+    <>
+    <Statistic name="good" value={good}/>
+    <Statistic name="neutral" value={neutral}/>
+    <Statistic name="bad" value={bad}/>
+    <Statistic name="all" value={all}/>
+    <Statistic name="avg" value={(good - bad) / all}/>
+    <Statistic name="positive %" value={(good / all) * 100}/>
+    </>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
-   
     <div>
-      {value}
-      <button onClick={() => setValue(0)}>reset to zero</button>
+      <Title name="give feedbacks"/>
+      <Button name="good" func={setGood} value={good}/>
+      <Button name="neutral" func={setNeutral} value={neutral}/>
+      <Button name="bad" func={setBad} value={bad}/>
+      <Title name="statistics"/>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
 
-export default App;
+export default App
