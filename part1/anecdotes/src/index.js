5de80c53ee86bc0c5e135d231182d611ff0eeb2d
jean bravo <jjbravo1996@gmail.com>
2021-11-10 11:30:45 +0100

    jeje

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,17 +1,85 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button =(props) => {
+  return(<button onClick={props.func}>{props.name}</button>)
+}
+
+const Ttitle = (props) =>{
+  return(<h1>{props.name}</h1>)
+}
+
+const Anecdote = (props) =>{
+  return(<p>{props.anecdote}</p>)
+}
+
+const Vote = (props) =>{
+  return(<p>anecdote has {props.vote} votes</p>)
+}
+
+const MostVote =(props)=>{
+  const countVotes = Math.max(...props.vote)
+  const indexOfMostVote = props.vote.indexOf(countVotes)
+  const ganador = props.anecdote[indexOfMostVote]
+  
+  if(countVotes===0){
+    return(
+      <p>No votes yet</p>
+    )
+  }
+  return(
+    
+    <div>
+     
+      <p>{ganador}</p>
+    <p>has {countVotes} votes</p>
+    </div>
+    
+  )
+}
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+  const handleSelected=()=>{
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+  
+  
+  const handleVote =()=>{
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+    
+  }
+  
+
+
+
+  return (
+    <div>
+      <Ttitle name="Anecdote of the day"/>
+      <Anecdote anecdote={props.anecdotes[selected]}/>
+      <Vote vote={votes[selected]}/>
+      <Button name="vote" func={handleVote}/>
+      <Button name="next anecdote" func={handleSelected}/>
+      <br/>
+      <Ttitle name="Anecote with most votes"/>
+      <MostVote vote={votes} anecdote={anecdotes}/>
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App anecdotes={anecdotes} />,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
